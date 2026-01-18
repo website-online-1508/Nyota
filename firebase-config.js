ec8bc06e9f6e296fa69219054c6857c845d4fdea
website-online-1508 <bossrono1508@gmail.com>
2026-01-18 04:39:24 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAthQbGd6MG40bzytcRi8sveLYb4jT8xME",
+  authDomain: "knec-48ac1.firebaseapp.com",
+  projectId: "knec-48ac1",
+  storageBucket: "knec-48ac1.appspot.com",
+  messagingSenderId: "884023224058",
+  appId: "1:884023224058:web:2c93e9ac4cb129f0b1cb43"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
